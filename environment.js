71af61c893c42eb83f9406b98069bdc4c5a54299
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2024-07-01 18:09:54 +0700

    create :  function loadTexture , mtl obj

--- environment.js
+++ environment.js
@@ -15,19 +15,58 @@ camera.position.set(0,0,40);
 camera.lookAt(0,0,0);
 
 //Texture Loader
-const textureLoader = new THREE.TextureLoader();
-textureLoader.load('floorTexture.jpg', function (texture){
-    texture.wrapS = THREE.RepeatWrapping;
-    texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set(10,10);
-    //Plane Lantai 
-    var planeGeo = new THREE.BoxGeometry(30,30,1, 5, 5); //w h d wS hS dS
-    var planeMat = new THREE.MeshBasicMaterial({map : texture});
-    var lantai = new THREE.Mesh(planeGeo, planeMat);
-    lantai.rotation.x += 1.6;
-    lantai.position.set(0,-5,0);
-    scene.add(lantai);
-});
+function loadTexture(img, wrapHorizontal, wrapVertical, Geo, Mat, position, rotation, scene) {
+    const textureLoader = new THREE.TextureLoader();
+    textureLoader.load(img, function (texture) {
+        texture.wrapS = THREE.RepeatWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+        texture.repeat.set(wrapHorizontal, wrapVertical);
+        
+        Mat.map = texture;  
+
+        const obj = new THREE.Mesh(Geo, Mat);
+        obj.position.set(position.x, position.y, position.z);
+        obj.rotation.set(rotation.x, rotation.y, rotation.z);
+        scene.add(obj);
+    });
+}
+
+//MTL Loader
+function loadMTL(path, mtl, targetObj, scale, position){
+new MTLLoader()
+    .setPath( path )
+        .load( mtl , function ( materials ) {
+
+                materials.preload();
+
+                new OBJLoader()
+                    .setMaterials( materials )
+                    .setPath( path )
+                    .load( mtl, function ( object ) {
+                        targetObj.add( object );
+                        object.scale.set(scale.x, scale.y, scale.z);
+                        object.position.set(position.x, position.y, position.z);
+
+                    }, );
+                } );
+}
+
+//Plane Lantai 
+const lantaiGeo = new THREE.BoxGeometry(30,30,1, 5, 5); //w h d wS hS dS
+const lantaiMat = new THREE.MeshBasicMaterial();
+const lantaiPos = {x:0, y:-5, z:0};
+const lantaiRot = {x:1.6, y:0, z:0};
+const lantaiImg = 'floorTexture.jpg'
+loadTexture(lantaiImg ,10, 10, lantaiGeo, lantaiMat, lantaiPos, lantaiRot, scene);
+
+//import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
+// let sparkle_mask;
+// const sparkle_mask_loader = new GLTFLoader();
+// sparkle_mask_loader.load('/assets/sparkle_mask/scene.gltf',(glscene)=>{
+//   sparkle_mask = glscene.scene;
+//   scene.add(sparkle_mask);
+//   sparkle_mask.position.set(5,0,0)
+// })
 
 
 //control camera
